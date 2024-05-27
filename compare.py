37f8454d63ec8219f7bf82b9b1ab5822476bee54
Sana <sanaq1992@live.com>
2024-05-27 18:33:38 -0500

    conditionals simplified

--- compare.py
+++ compare.py
@@ -1,10 +1,7 @@
 x = int(input("what's x?"))
 y = int(input("what's y?"))
 
-#boolean true or false answer
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
-elif x == y:
+if x != y:
+    print("x is not equal to y")
+else:
     print("x is equal to y")
