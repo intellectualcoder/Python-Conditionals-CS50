d8906b4dbc5b42b676797a186e156d7972754ce6
Sana <sanaq1992@live.com>
2024-06-07 19:27:50 -0500

    house.py

---
+++ house.py
@@ -0,0 +1,16 @@
+name = input("What's your name ?").lower()
+
+if name == "Harry":
+    print("Gryffindor")
+
+elif name == "Ron":
+    print("Gryffindor")
+
+elif name == "Hermione":
+    print("Gryffindor")
+
+elif name == "Ron":
+    print("Gryffindor")
+
+else:
+    print("Who?")
